d47d16dd6cfb21840c9bfbb7ff2949e0ed14686d
shishir19999 <shishirsharma199@gmail.com>
2025-01-23 20:59:53 +0545

    update

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,10 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base:"/VRIT-DragForm",
-  plugins: [react()],
-  
+  base: '/VRIT-DragForm/', 
+  build: {
+    outDir: 'dist',
+  },
+  plugins: [react()]
 });
+
